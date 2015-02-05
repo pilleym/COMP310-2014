b2bd5642288ab15405235d9c0a89f1fd5d5946d6
Melissa Pilley <pilleym@wit.edu>
2015-02-05 09:18:13 -0500

    BinaryTree

--- workspace/Example/src/BinaryTreeNode.java
+++ workspace/Example/src/BinaryTreeNode.java
@@ -7,26 +7,54 @@ public class BinaryTreeNode
 	BinaryTreeNode(DataType data) {
 		this.data = data;
 	}
-	
 	void add(DataType moreData) {
 		// figure out which side to place it
 		if (data.compareTo(moreData) <= 0) {
 			// it goes to the right
 			if (right == null) {
 				// create a node with the data and call it a day
+				right = new BinaryTreeNode<DataType>(moreData);
 			} else {
 				// keep looking recursively
+				right.add(moreData);
 			}
 		} else {
 			// it goes to the left
 			if (left == null) {
 				// create a node and call it a day
+				left = new BinaryTreeNode<DataType>(moreData);
 			} else {
 				// keep looking recursively
+				left.add(moreData);
 			}
 		}
-		// put it there
 	}
+	// Can you write this function?
+	// Hint: try doing what you do in the add function
+	boolean contains (DataType data) {
+		//if(this.data.compareTo(data){
+			int result = data.compareTo(data);
+			if (result < 0)
+				if(right ==null)
+				{
+					return false;
+				}
+				else if (result == 0){
+					return true;
+				}
+				else {
+					if(left == null){
+						return false;
+					}
+					else{
+						return left.contains(data);
+					}
+						
+			}
+			return false;
+		}
+		
+	
 	public static void main(String[] args) {
 		System.out.println("Hello".compareTo("World"));
 	}
